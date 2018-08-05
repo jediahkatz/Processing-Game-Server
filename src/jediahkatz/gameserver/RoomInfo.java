06f4218020cbc754a1a313c7f3b478af317ecb1b
Jediah Katz <jediahkatz@gmail.com>
2018-08-05 14:56:13 -0400

    Add wrapper class for getting room info

---
+++ src/jediahkatz/gameserver/RoomInfo.java
@@ -0,0 +1,56 @@
+package jediahkatz.gameserver;
+
+import processing.data.JSONObject;
+
+/**
+ * A wrapper class that holds information about a room.
+ * Data is only accurate up to the time the RoomInfo object was created.
+ * @author jediahkatz
+ *
+ */
+public class RoomInfo {
+	private final int id;
+	private final int capacity;
+	private final int size;
+	private final JSONObject attributes;
+	private final int[] clientIds;
+	
+	public RoomInfo(int id, int capacity, int size, JSONObject attributes, int[] clientIds) {
+		this.id = id;
+		this.capacity = capacity;
+		this.size = size;
+		this.attributes = attributes;
+		this.clientIds = clientIds;
+	}
+	
+	/** Get this room's unique id. **/
+	public int id() {
+		return id;
+	}
+	
+	/** Get the number of clients currently in this room. **/
+	public int size() {
+		return size;
+	}
+	
+	/** Get the maximum number of clients allowed in this room. **/
+	public int capacity() {
+		return capacity;
+	}
+	
+	/** Get an array containing the unique ids of all clients currently in this room.
+	 * Modifying this array will modify the array in this RoomInfo instance, but will
+	 * not modify the actual room in any way. 
+	**/
+	public int[] clients() {
+		return clientIds;
+	}
+	
+	/** Get a JSONObject containing the attributes for this room.
+	 * Modifying this JSONObject will modify the attributes in this RoomInfo
+	 * instance, but will not modify the actual room in any way. 
+	**/
+	public JSONObject attributes() {
+		return attributes;
+	}
+}
