f58dc92ce81c7c0558324bbe8a20b57de55dc307
Jediah Katz <jediahkatz@gmail.com>
2018-08-18 13:50:45 -0400

    Add Message convenience functions

--- src/jediahkatz/gameserver/Message.java
+++ src/jediahkatz/gameserver/Message.java
@@ -1,5 +1,6 @@
 package jediahkatz.gameserver;
 
+import processing.data.JSONArray;
 import processing.data.JSONObject;
 
 public class Message {
@@ -24,5 +25,45 @@ public class Message {
 	 */
 	public JSONObject getBody() {
 		return body;
+	}
+	
+	/** Convenience method for getBody().getString(key). **/
+	public String getString(String key) {
+		return body.getString(key);
+	}
+	
+	/** Convenience method for getBody().getInt(key). **/
+	public int getInt(String key) {
+		return body.getInt(key);
+	}
+	
+	/** Convenience method for getBody().getBoolean(key). **/
+	public boolean getBoolean(String key) {
+		return body.getBoolean(key);
+	}
+	
+	/** Convenience method for getBody().getJSONObject(key). **/
+	public JSONObject getJSONObject(String key) {
+		return body.getJSONObject(key);
+	}
+	
+	/** Convenience method for getBody().getJSONArray(key). **/
+	public JSONArray getJSONArray(String key) {
+		return body.getJSONArray(key);
+	}
+	
+	/** Convenience method for getBody().getFloat(key). **/
+	public float getFloat(String key) {
+		return body.getFloat(key);
+	}
+	
+	/** Convenience method for getBody().getDouble(key). **/
+	public double getDouble(String key) {
+		return body.getDouble(key);
+	}
+	
+	/** Convenience method for getBody().getLong(key). **/
+	public long getLong(String key) {
+		return body.getLong(key);
 	}
 }
